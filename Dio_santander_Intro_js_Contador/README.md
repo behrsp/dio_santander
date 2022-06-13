2331e34d73490c97949c6430e6dd4d6e30b87744
Robson <90429740+behrsp@users.noreply.github.com>
2022-06-13 12:38:03 -0300

    Add files via upload

---
+++ Dio_santander_Intro_js_Contador/README.md
@@ -0,0 +1,6 @@
+# Dio_santander_Intro_js
+Primeiro projeto introdução ao javascript - Contator
+- Fiz algumas modificações bem básicas no HTML e CSS por hora.
+- no html, acrescentei algumas classes para modificar o css ao meu gosto.
+- no css, adicionei aos botões a função hover.
+- Os próximos passos, serão alterar o JS.
---
+++ Dio_santander_Intro_js_Contador/teste.js
@@ -0,0 +1,16 @@
+//console.log("Hello world js");
+
+
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);     
+        }
+    }
+    console.log('Os numeros pares sao: ',evenNums);
+}
+
+let array = [1,2,4,5,7,8];
+returnEvenValues(array);
+
